0c2c73a399847e33226e55b33f2095414f41f586
idasamayram <81418105+idasamayram@users.noreply.github.com>
2022-04-25 23:38:24 +0200

    First Task

--- authentication.py
+++ authentication.py
@@ -3,7 +3,7 @@ from conf import *
 
 
 def authenticate():
-    login_url = base_url + '/p/login'   #POST/p/login: Requests Headers and forms the data parameters
+    login_url = base_url + '/p/login'   #POST/p/login: Requests headers and forms the data parameters
 
     data = {'name': username,'password': password,'tokenonly': 'true'}  #gets the data from conf
     #if the user is in multiple workspaces, this additional data is also being handed out to server:

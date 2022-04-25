7c8be9f3b30f386c096a333a89e74a298a90ddf1
idasamayram <81418105+idasamayram@users.noreply.github.com>
2022-04-25 23:36:43 +0200

    user variables

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-# user variables
+#user variables
 username = 'firstname.lastname@example.org' # email address you registered with
 password = 'mypassword' # your password
 base_url = 'https://editor.signavio.com' # target system url

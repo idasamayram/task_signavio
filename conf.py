fa7fb04028f20a17abf9380d44d449f4a442b21e
idasamayram <81418105+idasamayram@users.noreply.github.com>
2022-04-25 23:16:21 +0200

    Delete conf.py

--- conf.py
+++
@@ -1,6 +0,0 @@
-# user variables
-username = 'firstname.lastname@example.org' # email address you registered with
-password = 'mypassword' # your password
-base_url = 'https://editor.signavio.com' # target system url
-# provide if user is member in multiple workspaces:
-workspace_ID = '<workspace_ID>'
